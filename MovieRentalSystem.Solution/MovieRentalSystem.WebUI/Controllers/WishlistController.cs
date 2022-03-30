e3119d42a75af42b3d2c1ae7505c402538bb99ec
RahimliZakir <zakirer@code.edu.az>
2022-03-30 08:17:07 +0400

    Show Wishlist Written

--- MovieRentalSystem.Solution/MovieRentalSystem.WebUI/Controllers/WishlistController.cs
+++ MovieRentalSystem.Solution/MovieRentalSystem.WebUI/Controllers/WishlistController.cs
@@ -1,12 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MovieRentalSystem.WebUI.AppCode.Modules.ShowsModule;
+using MovieRentalSystem.WebUI.Models.Entities;
+using MovieRentalSystem.WebUI.Models.ViewModels;
+using System.Linq;
 
 namespace MovieRentalSystem.WebUI.Controllers
 {
+    [AllowAnonymous]
     public class WishlistController : Controller
     {
-        public IActionResult Index()
+        readonly IMediator mediator;
+
+        public WishlistController(IMediator mediator)
         {
-            return View();
+            this.mediator = mediator;
+        }
+
+        async public Task<IActionResult> Index()
+        {
+            WishlistViewModel? vm = new();
+
+            HttpContext.Request.Cookies.TryGetValue("wishlist-show", out string? wishlistShow);
+
+            if (!string.IsNullOrWhiteSpace(wishlistShow))
+            {
+                int[]? selectedShowIds = wishlistShow?.Split(new[] { ',' })
+                                                      .Select(b => int.Parse(b))
+                                                      .ToArray();
+
+                if (selectedShowIds?.Length > 0 && selectedShowIds != null)
+                {
+                    IEnumerable<Show>? shows = await mediator.Send(new ShowGetAllActiveQuery());
+                    shows = shows.Where(s => selectedShowIds.Contains(s.Id));
+                    vm.Shows = shows;
+                }
+            }
+
+            return View(vm);
         }
     }
 }
---
+++ MovieRentalSystem.Solution/MovieRentalSystem.WebUI/Models/ViewModels/WishlistViewModel.cs
@@ -0,0 +1,9 @@
+﻿using MovieRentalSystem.WebUI.Models.Entities;
+
+namespace MovieRentalSystem.WebUI.Models.ViewModels
+{
+    public class WishlistViewModel
+    {
+        public IEnumerable<Show>? Shows { get; set; }
+    }
+}

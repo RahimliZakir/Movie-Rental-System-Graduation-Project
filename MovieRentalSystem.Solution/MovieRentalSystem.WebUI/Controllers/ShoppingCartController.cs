1e81d1152445c4197c3515b71ea36a74ad24d78d
RahimliZakir <zakirer@code.edu.az>
2022-04-03 10:16:46 +0400

    Some Shopping Side Edits Yet

--- MovieRentalSystem.Solution/MovieRentalSystem.WebUI/Controllers/ShoppingCartController.cs
+++ MovieRentalSystem.Solution/MovieRentalSystem.WebUI/Controllers/ShoppingCartController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MovieRentalSystem.WebUI.Controllers
 {
     public class ShoppingCartController : Controller
     {
+        readonly IMediator mediator;
+
+        public ShoppingCartController(IMediator mediator)
+        {
+            this.mediator = mediator;
+        }
+
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();

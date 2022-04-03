4fe9ef55072008c0acf70ccdcb3e4f5d5f6d2197
RahimliZakir <zakirer@code.edu.az>
2022-04-03 08:32:30 +0400

    Movies & Shows Complex Cookie Done

--- MovieRentalSystem.Solution/MovieRentalSystem.WebUI/wwwroot/assets/js/header.js
+++ MovieRentalSystem.Solution/MovieRentalSystem.WebUI/wwwroot/assets/js/header.js
@@ -1,20 +1,26 @@
 window.addEventListener("load", function () {
-  let currentLocation = window.location.href;
-  let menus = document.querySelectorAll(".nav-links>li");
-  let collapsedMenus = document.querySelectorAll(".collapsed-nav-links>li");
-  let menusLength = menus.length;
+    let currentLocation = window.location.href;
+    let menus = document.querySelectorAll(".nav-links>li");
+    let collapsedMenus = document.querySelectorAll(".collapsed-nav-links>li");
+    let menusLength = menus.length;
 
-  for (let i = 0; i < menusLength; i++) {
-    if (
-      menus[i].children[0].href == currentLocation &&
-      collapsedMenus[i].children[0].href
-    ) {
-      menus[i].children[0].classList.add("active");
-      collapsedMenus[i].children[0].classList.add("active");
+    menus[0].children[0].classList.add("active");
+    collapsedMenus[0].children[0].classList.add("active");
+
+    for (let i = 0; i < menusLength; i++) {
+        if (
+            menus[i].children[0].href == currentLocation &&
+            collapsedMenus[i].children[0].href == currentLocation
+        ) {
+            menus[0].children[0].classList.remove("active");
+            collapsedMenus[0].children[0].classList.remove("active");
+
+            menus[i].children[0].classList.add("active");
+            collapsedMenus[i].children[0].classList.add("active");
+        }
     }
-  }
 
-  $(".right-nav-collapse-btn").on("click", () => {
-    $(".collapsed-nav-items").slideToggle();
-  });
+    $(".right-nav-collapse-btn").on("click", () => {
+        $(".collapsed-nav-items").slideToggle();
+    });
 });
